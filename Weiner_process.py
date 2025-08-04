243197a151c5fff290d9d9e1cf8f2f7ff431d831
gammlet <37348223+gammlet@users.noreply.github.com>
2025-08-04 15:28:48 +0300

    Create Weiner_process.py

---
+++ Weiner_process.py
@@ -0,0 +1,36 @@
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
+path = [1, -1]
+steps = 1000
+start_value = 100 
+values = []
+p = 0.5
+q = 1 - p
+probs = [p, q]
+
+for step in range(steps):
+    values.append(start_value)
+    
+    start_value = start_value + random.choice(path)
+    last_val = start_value
+
+time = [i for i in range(len(values))]
+
+
+
+eq_x = [0, steps]
+eq_y = [100, values[-1]]
+
+plt.figure(figsize=(14, 5))
+
+plt.plot(time, values)
+plt.plot(eq_x, eq_y, "k--")
+
+plt.ylabel("Value")
+plt.xlabel("Time")
+
+plt.show()
+
+
