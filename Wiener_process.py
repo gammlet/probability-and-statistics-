15a3db11b04b2eb6d349e58d650cccd4b08fd81c
gammlet <37348223+gammlet@users.noreply.github.com>
2025-08-05 13:55:06 +0300

    Update Wiener_process.py

--- Wiener_process.py
+++ Wiener_process.py
@@ -6,7 +6,7 @@ path = [1, -1]
 steps = 1000
 start_value = 100 
 values = []
-p = 0.5
+p = 0.8
 q = 1 - p
 probs = [p, q]
 
@@ -14,14 +14,15 @@ for step in range(steps):
     values.append(start_value)
     start_value = start_value + random.choices(path, weights=probs, k=1)[0]
     last_val = start_value
+
 time = [i for i in range(len(values))]
 spaced_eq_x =  np.linspace(0, steps - 1, len(values)) 
 spaced_eq_y =  np.linspace(100, values[-1], len(values)) 
 
 def calc_diff():
-    diff_list = np.array(spaced_eq_y) - np.array(values)
+    diff_list =np.array(values) - np.array(spaced_eq_y)
     return diff_list 
-    
+
 def show_plot(diff_list):
     plt.figure(figsize=(14, 5))
     plt.subplot(2, 1 ,1)
@@ -36,7 +37,7 @@ def show_plot(diff_list):
     plt.legend()
     plt.grid()
     plt.show()
-    
+
 print(calc_diff())
 show_plot(calc_diff())
 
