15ab56b1067589b20e7f9450a60cec778d17d41c
gammlet <37348223+gammlet@users.noreply.github.com>
2025-08-05 12:53:40 +0300

    Update Wiener_process.py

--- Wiener_process.py
+++ Wiener_process.py
@@ -1,9 +1,9 @@
 import random
 import matplotlib.pyplot as plt
-#import numpy as np
+import numpy as np
 
 path = [1, -1]
-steps = 1000
+steps = 10
 start_value = 100 
 values = []
 #p = 0.5
@@ -18,17 +18,26 @@ for step in range(steps):
 
 time = [i for i in range(len(values))]
 
-eq_x = [0, steps-1]
-eq_y = [100, values[-1]]
 
-plt.figure(figsize=(14, 5))
+spaced_eq_x =  np.linspace(0, steps - 1, len(values)) 
+spaced_eq_y =  np.linspace(100, values[-1], len(values)) 
 
-plt.plot(time, values)
-plt.plot(eq_x, eq_y, "k--")
 
-plt.ylabel("Value")
-plt.xlabel("Time")
+def calc_diff():
+    diff_list = []
+    for i in range(len(spaced_eq_y)):
+        difference = spaced_eq_y[i] - values[i]
+        diff_list.append(difference)
+    return diff_list 
 
-plt.show()
+def show_plot():
+    plt.figure(figsize=(14, 5))
+    plt.plot(time, values)
+    plt.plot(spaced_eq_x, spaced_eq_y, "k--")
+    plt.ylabel("Value")
+    plt.xlabel("Time")
+    plt.show()
 
+print(calc_diff())
+show_plot()
 
